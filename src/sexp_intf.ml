16ed74eb78c67e76e6cf9219c1b06b93b8c01c53
public-release <as-public-release@janestreet.com>
2026-01-14 01:07:05 +0000

    v0.18~preview.130.83+317

--- src/sexp_intf.ml
+++ src/sexp_intf.ml
@@ -514,7 +514,11 @@ module type S = sig
 
   include Pretty_printing with type output := string (** @inline *)
 
-  (** See [Pretty_printing.to_string_mach] and [to_string], respectively. *)
+  (** See [Pretty_printing.to_string_hum], [to_string_mach], and [to_string],
+      respectively. *)
+
+  (** WARNING [to_string_hum__stack] globalizes [t] if it is allocated on the stack. *)
+  val to_string_hum__stack : ?indent:int -> ?max_width:int -> t -> string
 
   val to_string_mach__stack : t -> string
   val to_string__stack : t -> string
